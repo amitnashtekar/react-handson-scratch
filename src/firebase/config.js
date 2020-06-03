6aabbfccec8ae7a92d3b37c4ae4f860f6cf81f17
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-06-04 01:38:03 +1000

    singin, sign up functionality, react-hook-form, custom components

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,3 @@
 export default {
-    apiKey: "AIzaSyAWM1xlY08C02Toyxyt_NhPcqrCp2JBRh8",
-    authDomain: "react-shoppy.firebaseapp.com",
-    databaseURL: "https://react-shoppy.firebaseio.com",
-    projectId: "react-shoppy",
-    storageBucket: "react-shoppy.appspot.com",
-    messagingSenderId: "963627332874",
-    appId: "1:963627332874:web:f2db6620b85215b64876f3"
+   
 }
